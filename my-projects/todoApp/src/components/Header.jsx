ef2f5565e1f5e5a813f1512bb97cf943f0a5b888
Abdul Rafay <rafay.3587@gmail.com>
2025-09-22 17:34:13 +0500

    Sorry, React, Pocketbase (not supabase) todo app

--- my-projects/todoApp/src/components/Header.jsx
+++ my-projects/todoApp/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
         <header className="bg-blue-800 px-2 py-6 text-center">
-            <h1 className="text-4xl text-white font-bold"></h1>
+            <h1 className="text-4xl text-white font-bold">React, PocketBase and Tailwind</h1>
         </header>
     );
 }
